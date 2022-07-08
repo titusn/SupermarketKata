1aa0becc413cdd8c9dbdccf9fc4c8d9ed23aca8e
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-07-08 16:42:12 +0200

    Remove double primitive from Money

--- src/main/java/com/titusnachbauer/supermarket/Money.java
+++ src/main/java/com/titusnachbauer/supermarket/Money.java
@@ -4,16 +4,10 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Money {
-    final double amount;
     BigDecimal bigDecimalAmount;
 
     Money(double amount) {
-        this.amount = amount;
         this.bigDecimalAmount = BigDecimal.valueOf(amount);
-    }
-
-    public double getAmount() {
-        return amount;
     }
 
     public BigDecimal getBigDecimalAmount() {
