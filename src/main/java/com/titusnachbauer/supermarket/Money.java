f26c49b7c6b655b6c466e1f60ef668a7cf46cd78
BVabn <95869313+BVabn@users.noreply.github.com>
2022-07-14 16:04:41 +0200

    Add toString()

--- src/main/java/com/titusnachbauer/supermarket/Money.java
+++ src/main/java/com/titusnachbauer/supermarket/Money.java
@@ -1,6 +1,9 @@
 package com.titusnachbauer.supermarket;
 
 import java.math.BigDecimal;
+import java.util.Locale;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class Money {
     private final BigDecimal amount;
@@ -12,4 +15,12 @@ public class Money {
     public BigDecimal getAmount() {
         return amount;
     }
+
+    public String toString() {
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.GERMAN);
+        DecimalFormat df = new DecimalFormat("#.##", otherSymbols);
+        df.setMinimumFractionDigits(2);
+        return df.format(amount.doubleValue());
+    }
+
 }
--- src/test/java/com/titusnachbauer/supermarket/MoneyTest.java
+++ src/test/java/com/titusnachbauer/supermarket/MoneyTest.java
@@ -13,4 +13,19 @@ public class MoneyTest {
         Money money = new Money(42.00);
         assertThat(money.getAmount(), equalTo(BigDecimal.valueOf(42)));
     }
+
+    @Test
+    void GivenNewMoneyWhenGettingValueThenReturnsStringValue() {
+        Money money = new Money(42.00);
+        assertThat(money.toString(), equalTo("42.00"));
+    }
+
+    // @Test
+    // void GivenTwoMoney5And7AddTogetherAndGet12() {
+    //     Money money1 = new Money(5.00);
+    //     Money money2 = new Money(7.00);
+    //     money1.add(money2);
+    //     assertThat(money1.getAmount(), equalTo(BigDecimal.))
+    // }
+
 }
