dc7f835fb0b73da0d8e0d077c6e74be384458eca
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-07-21 14:57:59 +0200

    Add test for add() method to Money

--- src/test/java/com/titusnachbauer/supermarket/MoneyTest.java
+++ src/test/java/com/titusnachbauer/supermarket/MoneyTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class MoneyTest {
+class MoneyTest {
     @Test
     void GivenNewMoneyWhenGettingValueThenReturnsValue() {
         Money money = new Money(42.00);
@@ -21,7 +21,7 @@ public class MoneyTest {
     }
 
      @Test
-     void GivenTwoMoney5And7AddTogetherAndGet12() {
+     void GivenTwoMoneyWhenAddingUpThenTotalIsCorrect() {
          Money money1 = new Money(5.00);
          Money money2 = new Money(7.00);
          money1.add(money2);
